bfd8d11659381b78d17be68ca76125bb59a7a7ae
pooneth <jtharel@yahoo.com>
2020-10-29 09:33:17 -0600

    Add files via upload

---
+++ readtable.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+conn = sqlite3.connect('OrdersMobile.sqlite')
+
+c = conn.cursor()
+d = conn.cursor()
+
+sql = "SELECT name FROM sqlite_master WHERE type='table';"
+for row in c.execute(sql):
+	print("Table: " + str(row)[3:-3])
+	table_name = str(row)[3:-3]
+
+	newsql = "SELECT * FROM " + table_name + ";"
+	print("SQL Statement = " + newsql)
+	for row in d.execute(newsql):
+		print(str(row))
+	conn.commit()
+	print("   ")
+	print("**********")
+	print("   ")
+
+		
+conn.commit()
+
+
+conn.close()
+
