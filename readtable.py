366c1a9169ce567e439f9af29098950c2b77e3b3
pooneth <jtharel@yahoo.com>
2021-05-12 14:36:25 -0600

    Update readtable.py
    added command line option to read the db

--- readtable.py
+++ readtable.py
@@ -1,27 +1,42 @@
 import sqlite3
+import sys
 
-conn = sqlite3.connect('DBName.sqlite')
+def Help():
+	print("Usage:")
+	print("  python ./readtable.py [tablename]")
+	print(" ")
+	print("  python ./readtable.py users")
 
-c = conn.cursor()
-d = conn.cursor()
 
-sql = "SELECT name FROM sqlite_master WHERE type='table';"
-for row in c.execute(sql):
-	print("Table: " + str(row)[3:-3])
-	table_name = str(row)[3:-3]
+def dumptables(dbname):
 
-	newsql = "SELECT * FROM " + table_name + ";"
-	print("SQL Statement = " + newsql)
-	for row in d.execute(newsql):
-		print(str(row))
+	conn = sqlite3.connect(dbname)
+
+	c = conn.cursor()
+	d = conn.cursor()
+
+	sql = "SELECT name FROM sqlite_master WHERE type='table';"
+	for row in c.execute(sql):
+		print("Table: " + str(row)[3:-3])
+		table_name = str(row)[3:-3]
+
+		newsql = "SELECT * FROM " + table_name + ";"
+		print("SQL Statement = " + newsql)
+		for row in d.execute(newsql):
+			print(str(row))
+		conn.commit()
+		print("   ")
+		print("**********")
+		print("   ")
+	
 	conn.commit()
-	print("   ")
-	print("**********")
-	print("   ")
-
-		
-conn.commit()
+	conn.close()
 
 
-conn.close()
+if len(sys.argv) == 1:
+	Help()
+	sys.exit
+else:
+	dbname = sys.argv[1]
+	dumptables(dbname)
 
